e16b4a59ca86aef761c2fd3b2fb81964bd630c49
yannickoo <drupal@yannickoo.de>
2014-08-14 03:13:30 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+var app = angular.module('contributions', []);
+
+app.controller('ContributionController', function($scope, $http) {
+  $scope.messages = {};
+  $scope.options = {
+    username: 'yannickoo',
+  };
+
+  $scope.user = {};
+
+  $scope.updateUsername = function() {
+    $http.get('https://api.github.com/users/' + $scope.options.username).success(function(user) {
+      $scope.messages = {};
+      $scope.user = user;
+      $scope.user.name = typeof user.name !== 'undefined' ? user.name : user.login;
+
+      $http.get(user.repos_url).success(function(repos) {
+        if (!repos.length) {
+          $scope.options.username = '';
+          $scope.messages.error = $scope.user.name + ' has no repositories yet.';
+
+          return;
+        }
+
+        $scope.user.repos = repos;
+      });
+    }).error(function(data, status) {
+      $scope.messages.error = data.message + ' (' + status + ')';
+      $scope.user = {};
+      $scope.options.username = '';
+    });
+  };
+
+});
