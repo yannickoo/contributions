8582fae6ac876fe0ce6a58c3a129c74f6c5698c6
yannickoo <drupal@yannickoo.de>
2015-01-30 14:24:06 +0100

    implements new github lib, fixes #1

--- js/app.js
+++ js/app.js
@@ -1,46 +1,72 @@
 var app = angular.module('contributions', []);
 
+app.directive('selectOnClick', function() {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      element.on('click', function() {
+        debugger;
+        this.select();
+      });
+    }
+  };
+});
+
 app.controller('ContributionController', function($scope, $http) {
+  $scope.user = {};
   $scope.messages = {};
   $scope.options = {
     query: '',
     username: 'yannickoo',
   };
 
-  $scope.user = {};
+  $scope.github = new Github({
+    token: 'e9e54d959a6a2dcb801f5ca0f58d33a32882b4cf',
+  });
 
-  $scope.updateUsername = function() {
-    $http.get('https://api.github.com/users/' + $scope.options.username).success(function(user) {
-      $scope.messages = {};
-      $scope.user = user;
-      $scope.user.name = typeof user.name !== 'undefined' ? user.name : user.login;
-
-      $http.get(user.repos_url).success(function(repos) {
-        if (!repos.length) {
-          $scope.options.username = '';
-          $scope.messages.error = $scope.user.name + ' has no repositories yet.';
-
-          return;
-        }
-
-        $scope.user.repos = repos;
-      });
-
-      $http.get('https://api.github.com/users/' + $scope.options.username + '/starred').success(function(repos) {
-        if (!repos.length) {
-          return;
-        }
-
-        $scope.user.starred = repos;
-      });
-    }).error(function(data, status) {
-      $scope.messages.error = data.message + ' (' + status + ')';
-      $scope.user = {};
-      $scope.options.username = '';
-    });
-  };
+  $scope.githubUser = $scope.github.getUser();
 
   $scope.hideZero = function(number) {
     return number > 0 ? number : '';
+  };
+
+  $scope.updateUsername = function() {
+    if (typeof $scope.user.name !== 'undefined' && $scope.user.name === $scope.options.username) {
+      return;
+    }
+
+    $scope.githubUser.show($scope.options.username, function(err, res) {
+      if (err) {
+        $scope.messages.error = err.message + ' (' + err.status + ')';
+        $scope.user = {};
+        $scope.options.username = '';
+      }
+      else {
+        $scope.user = res;
+        $scope.user.name = typeof res.name !== 'undefined' ? res.name : res.login;
+
+        $scope.githubUser.userRepos($scope.options.username, function(err, res) {
+          if (err) {
+            $scope.messages.error = data.message + ' (' + status + ')';
+            $scope.options.username = '';
+          }
+          else {
+            $scope.messages = {};
+            $scope.user.repos = res;
+          }
+        });
+
+        $scope.githubUser.userStarred($scope.options.username, function(err, res) {
+          if (err) {
+            $scope.messages.error = data.message + ' (' + status + ')';
+            $scope.options.username = '';
+          }
+          else {
+            $scope.messages = {};
+            $scope.user.starred = res;
+          }
+        });
+      }
+    });
   };
 });
